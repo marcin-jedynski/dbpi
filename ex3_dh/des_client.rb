b98b68b1b72b65c98fdb19bec277cb108bebb19c
Marcin Jedynski <marcin.jedynski@cognifide.com>
2018-11-29 15:56:43 +0100

    diffie-hellman + dsa + des

--- ex3_dh/des_client.rb
+++ ex3_dh/des_client.rb
@@ -1,6 +1,12 @@
 require 'openssl'
 require 'socket'
 
+hostname = 'localhost'
+port = 2000
+dsa = OpenSSL::PKey::DSA.new(2048)
+File.open('pub_key.txt', 'w') { |file| file.write(dsa.public_key.to_der) }
+# puts dsa.params
+
 def primitive_root()
     while (true)
         q = OpenSSL::BN.generate_prime(64)
@@ -26,32 +32,33 @@ private_rand = OpenSSL::BN.new(rand(2..p-1))
 
 #generate public key
 client_public = g.mod_exp(private_rand,p)
-# test = Marshal.dump(["msg" => "INIT","PRIME" => p.to_i,"GEN" => g.to_i,"CLIENT_PUB" => client_public.to_i ])
-# puts test
 ##send p,g,client_public
-hostname = 'localhost'
-port = 2000
-puts p,g
+
 s = TCPSocket.open(hostname, port)
-s.puts(Marshal.dump(["INIT",p.to_s, g.to_s, client_public.to_s ]))
+
+params = Marshal.dump(["INIT",p.to_s, g.to_s, client_public.to_s])
+hash = OpenSSL::Digest::SHA256.digest(params)
+signed = dsa.syssign(hash)
+puts "signature:"
+puts signed
+puts "hash:"
+puts hash
+s.puts(Marshal.dump([signed, params]))
 s.flush
-server_public = Marshal.load(s.gets)
-puts server_public
+server_public = OpenSSL::BN.new(Marshal.load(s.gets))
+secret = server_public.mod_exp(private_rand,p) 
 
+cipher = OpenSSL::Cipher.new('des')
+puts("Provide your message:")
+message = gets()
 
-# cipher = OpenSSL::Cipher.new('des')
-# puts("Provide your message:")
-# message = gets()
-# cipher.encrypt
-# key = cipher.random_key
-# iv = cipher.random_iv
-# encrypted = cipher.update(message) + cipher.final
-# #puts(encrypted)
-
-
-
-# s = TCPSocket.open(hostname, port)
-# s.write(Marshal.dump([p,g]))
-# s.write(Marshal.dump([key, iv, encrypted]))
-# s.flush
+cipher.encrypt
+cipher.key = secret.to_s.bytes[0..7].pack("c*")
+iv = cipher.random_iv
+encrypted = cipher.update(message) + cipher.final
+puts("message encrypted")
+puts(encrypted)
+s.flush
+s.puts(Marshal.dump([iv, encrypted]))
+s.flush
 s.close
--- ex3_dh/des_server.rb
+++ ex3_dh/des_server.rb
@@ -4,29 +4,45 @@ require 'openssl'
 decipher = OpenSSL::Cipher.new('des')
 decipher.decrypt
 
+
 server = TCPServer.open(2000)
 loop{
-
+ 
   client = server.accept
+  puts("client connection:")
   puts client
-  # array = Marshal.load(client.read)
-  # key, iv, message = array
+
+  client_key = File.read('pub_key.txt')
+  dsa = OpenSSL::PKey::DSA.new(client_key)
+
   parameters = Marshal.load(client.gets)
-  puts parameters[0]
+  signature = parameters[0]
+  hash = OpenSSL::Digest::SHA256.digest(parameters[1])
+  puts "signature:"
+  puts signature
+  puts "hash:"
+  puts hash
+  puts "Is signature veryfied: " + "#{dsa.sysverify(hash, signature)}"
+  parameters = Marshal.load(parameters[1])
+
   p = OpenSSL::BN.new(parameters[1])
   g = OpenSSL::BN.new(parameters[2])
   client_public = OpenSSL::BN.new(parameters[3])
   private_rand = OpenSSL::BN.new(rand(2..p-1))
   server_public = g.mod_exp(private_rand,p)
-  puts server_public
+
   client.puts(Marshal.dump(server_public.to_s))
-  
-  # client.flush
 
-
-  # #puts(key, iv, message)
-  # decipher.key = key
-  # decipher.iv = iv
-  # puts(decipher.update(message) + decipher.final)
+  secret = client_public.mod_exp(private_rand,p) 
+  client.flush
+  ciphertext = Marshal.load(client.gets)
+  iv = ciphertext[0]
+  message = ciphertext[1]
+  decipher.key = secret.to_s.bytes[0..7].pack("c*")
+  decipher.iv = iv
+  puts("message encrypted:")
+  puts(message)
+  puts("message decrypted")
+  puts(decipher.update(message) + decipher.final)
   client.close
 }
